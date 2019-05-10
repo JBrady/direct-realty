a341df296946a6abfc4b4f6cb35b926582312858
John Brady <jbradysd@icloud.com>
2019-05-09 23:42:07 -0700

    Changing the default index page to the hosting template

--- packages/landing/pages/hosting.js
+++ packages/landing/pages/hosting.js
@@ -9,18 +9,11 @@ import {
 } from 'common/src/containers/Hosting/hosting.style';
 import { ResetCSS } from 'common/src/assets/css/style';
 import Navbar from 'common/src/containers/Hosting/Navbar';
-// import FeatureSection from 'common/src/containers/Hosting/Features';
 import InfoSection from 'common/src/containers/Hosting/Info';
 import DomainSection from 'common/src/containers/Hosting/Domain';
-// import PaymentSection from "common/src/containers/Hosting/Payment";
-// import GuaranteeSection from "common/src/containers/Hosting/Guarantee";
-// import FaqSection from "common/src/containers/Hosting/Faq";
-// import ServicesSection from "common/src/containers/Hosting/Services";
 import BannerSection from 'common/src/containers/Hosting/Banner';
-// import PricingSection from "common/src/containers/Hosting/Pricing";
 import TestimonialSection from 'common/src/containers/Hosting/Testimonials';
 import TeamSection from 'common/src/containers/Hosting/TeamSection';
-//import ContactSection from "common/src/containers/Hosting/Contact";
 import Footer from 'common/src/containers/Hosting/Footer';
 import { DrawerProvider } from 'common/src/contexts/DrawerContext';
 import { ParallaxProvider } from 'react-scroll-parallax';
@@ -54,15 +47,10 @@ export default () => {
 
           <BannerSection />
           <TeamSection />
-          {/* <FeatureSection /> */}
+
           <InfoSection />
           <DomainSection />
-          {/* <PricingSection />
-          <ServicesSection />
-          <PaymentSection />
-          <GuaranteeSection />
-          <FaqSection /> 
-          <ContactSection /> */}
+
           <TestimonialSection />
           <Footer />
         </ContentWrapper>
