5db35d90d61b8cf5b60ef444d7a6309a656d61dc
John Brady <jbradysd@icloud.com>
2019-06-05 16:59:45 -0700

    Updating meta description, page title, .gitignore file

--- packages/landing/pages/index.js
+++ packages/landing/pages/index.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import Head from 'next/head';
-import { ThemeProvider } from 'styled-components';
-import Sticky from 'react-stickynode';
-import { hostingTheme } from 'common/src/theme/hosting';
+import React from "react";
+import Head from "next/head";
+import { ThemeProvider } from "styled-components";
+import Sticky from "react-stickynode";
+import { hostingTheme } from "common/src/theme/hosting";
 import {
   GlobalStyle,
-  ContentWrapper,
-} from 'common/src/containers/Hosting/hosting.style';
-import { ResetCSS } from 'common/src/assets/css/style';
-import Navbar from 'common/src/containers/Hosting/Navbar';
-import InfoSection from 'common/src/containers/Hosting/Info';
-import DomainSection from 'common/src/containers/Hosting/Domain';
-import BannerSection from 'common/src/containers/Hosting/Banner';
-import TestimonialSection from 'common/src/containers/Hosting/Testimonials';
-import TeamSection from 'common/src/containers/Hosting/TeamSection';
-import Footer from 'common/src/containers/Hosting/Footer';
-import { DrawerProvider } from 'common/src/contexts/DrawerContext';
-import { ParallaxProvider } from 'react-scroll-parallax';
+  ContentWrapper
+} from "common/src/containers/Hosting/hosting.style";
+import { ResetCSS } from "common/src/assets/css/style";
+import Navbar from "common/src/containers/Hosting/Navbar";
+import InfoSection from "common/src/containers/Hosting/Info";
+import DomainSection from "common/src/containers/Hosting/Domain";
+import BannerSection from "common/src/containers/Hosting/Banner";
+import TestimonialSection from "common/src/containers/Hosting/Testimonials";
+import TeamSection from "common/src/containers/Hosting/TeamSection";
+import Footer from "common/src/containers/Hosting/Footer";
+import { DrawerProvider } from "common/src/contexts/DrawerContext";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 export default () => {
   return (
@@ -24,12 +24,11 @@ export default () => {
       <ParallaxProvider>
         <Head>
           <title>
-            Direct Realty | You deserve the best, expect the best, now
-            experience the best
+            San Diego, CA Real Estate | San Diego Homes for Sale | Direct Realty
           </title>
           <meta
             name="Description"
-            content="Our team focuses on taking the pressure off of you so you can make the best decisions for you and your family. We are there for you 24/7 – Call or email us anytime!"
+            content="View homes for sale in San Diego, CA. See pricing and listing details of San Diego real estate for sale."
           />
           <meta name="theme-color" content="#eb4d4b" />
           <link
